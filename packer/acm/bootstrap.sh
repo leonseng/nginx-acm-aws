e0bedc729a624b63606ef900e8679664b5d60c67
Leon Seng <w.leon.seng@gmail.com>
2022-09-13 20:28:33 +1000

    Refactored acm bootstrap script for easier troubleshooting

--- packer/acm/bootstrap.sh
+++ packer/acm/bootstrap.sh
@@ -7,26 +7,37 @@ sudo mkdir /etc/ssl/nginx
 sudo cp /tmp/nginx-repo.crt /etc/ssl/nginx/
 sudo cp /tmp/nginx-repo.key /etc/ssl/nginx/
 
-echo "Install Clickhouse"
+echo "Update apt repos"
+sudo apt-get update
+
+echo "Add Clickhouse repo"
 sudo apt-get install -y apt-transport-https ca-certificates dirmngr
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 8919F6BD2B48D754
 echo "deb https://packages.clickhouse.com/deb stable main" | sudo tee /etc/apt/sources.list.d/clickhouse.list
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y clickhouse-server clickhouse-client
-sudo service clickhouse-server start
-sudo systemctl enable clickhouse-server
-
-echo "Install NGINX OSS"
-sudo apt-get install -y nginx
 
 echo "Add NMS repo"
 printf "deb https://pkgs.nginx.com/nms/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee /etc/apt/sources.list.d/nms.list
 sudo wget -q -O /etc/apt/apt.conf.d/90pkgs-nginx https://cs.nginx.com/static/files/90pkgs-nginx
 sudo wget https://cs.nginx.com/static/keys/nginx_signing.key && sudo apt-key add nginx_signing.key
 
-echo "Install NIM and ACM"
+echo "Update apt repos"
 sudo apt-get update
-sudo apt-get install -y nms-instance-manager nms-api-connectivity-manager
+
+echo "Install Clickhouse"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y clickhouse-server clickhouse-client
+
+echo "Start Clickhouse"
+sudo service clickhouse-server start
+sudo systemctl enable clickhouse-server
+
+echo "Install NGINX OSS"
+sudo apt-get install -y nginx
+
+echo "Install NIM"
+sudo apt-get install -y nms-instance-manager
+
+echo "Install ACM"
+sudo apt-get install -y nms-api-connectivity-manager
 
 echo "Enable & start NMS services"
 sudo systemctl start nms
